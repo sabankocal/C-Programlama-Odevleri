b0558ebcda8b20469e6654b462679342e80b878e
saritepeci <105279573+saritepeci@users.noreply.github.com>
2022-07-18 11:54:11 +0200

    Update 30_Pointers.c

--- 30_Pointers/30_Pointers.c
+++ 30_Pointers/30_Pointers.c
@@ -1,14 +1,14 @@
-  Q: What is the wild pointer?
+  1. What is the wild pointer?
 
-  Q) What is dangling pointer in C? (Bunu henuz anlatmadim, inanilmaz onemli bir mevzu ayni zamanda cok populer bir mulakat sorusu)
+  2. What is dangling pointer in C? (Bunu henuz anlatmadim, inanilmaz onemli bir mevzu ayni zamanda cok populer bir mulakat sorusu)
 
-    Q) What is the difference between pass by value by reference in c and pass by reference in c?
+  3. What is the difference between pass by value by reference in c and pass by reference in c?
 
-    Q) What is the difference between an uninitialized pointer and a null pointer?
+  4. What is the difference between an uninitialized pointer and a null pointer?
         (Hatirlatin null pointer i derste anlatayim!)
 
-    Q) What is the FILE pointer?
+  5. What is the FILE pointer?
 
-    Q) What is segmentation fault in C?
+  6. What is segmentation fault in C?
     
     
