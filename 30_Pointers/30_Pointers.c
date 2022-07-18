56dba9c3f05dd25e9b9fdaa59f1a9c2cba41e31b
saritepeci <105279573+saritepeci@users.noreply.github.com>
2022-07-18 11:53:05 +0200

    Create 30_Pointers.c

---
+++ 30_Pointers/30_Pointers.c
@@ -0,0 +1,14 @@
+  Q: What is the wild pointer?
+
+  Q) What is dangling pointer in C? (Bunu henuz anlatmadim, inanilmaz onemli bir mevzu ayni zamanda cok populer bir mulakat sorusu)
+
+    Q) What is the difference between pass by value by reference in c and pass by reference in c?
+
+    Q) What is the difference between an uninitialized pointer and a null pointer?
+        (Hatirlatin null pointer i derste anlatayim!)
+
+    Q) What is the FILE pointer?
+
+    Q) What is segmentation fault in C?
+    
+    
