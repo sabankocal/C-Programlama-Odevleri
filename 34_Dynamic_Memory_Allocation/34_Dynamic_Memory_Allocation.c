92455dc68bdc568ec9866cf3374dad45193c6e7c
saritepeci <105279573+saritepeci@users.noreply.github.com>
2022-07-28 15:49:01 +0200

    Create 34_Dynamic_Memory_Allocation.c

---
+++ 34_Dynamic_Memory_Allocation/34_Dynamic_Memory_Allocation.c
@@ -0,0 +1,25 @@
+Interview questions about Dynamic Memory Allocation:
+
+Question 1: What is the difference between malloc and calloc?
+
+Question 2: Is it better to use malloc () or calloc ()?
+
+Question 3: What is the purpose of realloc( )?
+
+Question 4: What is static memory allocation and dynamic memory allocation? 
+            Give examples;
+
+Question 5: What is the return value of malloc (0)?
+
+Question 6: What is the memory leak in C?
+
+Question 7: What is a dangling(Zombie) pointer?
+
+Question 8: What is the problem of using dynamic memory allocation?
+
+Question 9: Can I increase the size of dynamically allocated memory? 
+            What about statically allocated memory?
+
+Question 10: What is the difference in execution speed when using dynamic memory allocation vs.
+              when using static memory allocation? Which memory regions responsible for this kind of allocations?
+
